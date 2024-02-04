a942c8a2a0f06ea8ad0c95f111078f52bcc51cc8
Raza Ali Rahman <aryanrahman90277@gmail.com>
2024-02-04 18:55:44 +0530

    create password change completed

--- src/app/api/create-password/route.ts
+++ src/app/api/create-password/route.ts
@@ -6,7 +6,30 @@ Connection();
 
 export async function POST(request: NextRequest) {
     try {
-        
+        const req = await request.json();
+        const { password, confirmPassword, resetId } = req;
+
+        const selectResetId = await User.findOne({ reset_password_token: resetId });
+
+        if(!password || !confirmPassword) {
+            return NextResponse.json({ message: "The above fields are required" });
+        }
+        else if(!resetId || !selectResetId) {
+            return NextResponse.json({ message: "The reset id is invalid" });
+        }
+        else if(password !== confirmPassword) {
+            return NextResponse.json({ message: "Password does'nt match" });
+        }
+        else {
+            const updatePassword = await User.findOneAndUpdate(
+                {
+                    password: password,
+                    reset_password_token: ""
+                }
+            );
+            return NextResponse.json({ message: "Password updated successfully", user: updatePassword });
+        }
+
     } catch (error) {
         console.error(error);
     }
