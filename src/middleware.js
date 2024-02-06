4e0e6c653e272dde9ee6d6f6485ad37426111b9b
Raza Ali Rahman <aryanrahman90277@gmail.com>
2024-02-06 17:24:00 +0530

    defined middlerware functions for the admin panel

--- src/middleware.js
+++ src/middleware.js
@@ -3,19 +3,38 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request) {
   const path = request.nextUrl.pathname;
 
-  const token = request.cookies.get("user_token")?.value || "";
-  const isPublicPath =
-    path === "/login" ||
-    path === "/signup" ||
-    path === "/forgot-password" ||
-    path === "/email-verification";
-    
-  const isProtectedPath = path === "/profile";
+  const userToken = request.cookies.get("user_token")?.value || "";
+  const adminToken = request.cookies.get("admin_token")?.value || "";
 
-  if (!isPublicPath && !token) {
+  const isPublicPath = [
+    "/login",
+    "/signup",
+    "/forgot-password",
+    "/email-verification",
+    "/admin/login",
+  ].includes(path);
+
+  // Define protected paths for regular users and admins
+  const isUserProtectedPath = path === "/profile";
+  const isAdminProtectedPath = path === "/admin/dashboard";
+
+  // Redirect logic for non-authenticated access to protected paths
+  if (isUserProtectedPath && !userToken) {
     return NextResponse.redirect(new URL("/login", request.url));
-  } else if (isPublicPath && token) {
+  }
+
+  if (isAdminProtectedPath && !adminToken) {
+    return NextResponse.redirect(new URL("/admin/login", request.url));
+  }
+
+  // Redirect authenticated users trying to access public paths
+  if (isPublicPath && userToken && !path.startsWith("/admin")) {
     return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  // Redirect authenticated admins trying to access the admin login page directly
+  if (path === "/admin/login" && adminToken) {
+    return NextResponse.redirect(new URL("/admin/dashboard", request.url));
   }
 }
 
@@ -27,5 +46,8 @@ export const config = {
     "/signup",
     "/forgot-password",
     "/email-verification",
+    "/create-password",
+    "/admin/login",
+    "/admin/dashboard",
   ],
 };
