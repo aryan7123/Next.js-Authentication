09d890a46566b2b52b37b1464d7d1569672d33b2
Raza Ali Rahman <aryanrahman90277@gmail.com>
2024-02-08 23:44:25 +0530

    created the first conatiner of the dashboard

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -5,6 +5,9 @@ import Loader from '@/app/components/Loader'
 import Navbar from "@/app/components/Navbar"
 
 import { useGlobalContext } from '@/app/components/Context'
+import { LuNetwork } from "react-icons/lu";
+import { FaChromecast, FaUserShield } from "react-icons/fa";
+import { RiCellphoneLine } from "react-icons/ri";
 
 const page = () => {
   const { loading, handleLoader, adminDetails, getAdminDetails, openSidebar } = useGlobalContext();
@@ -26,8 +29,38 @@ const page = () => {
             <Navbar />
             <div className='w-[inherit] h-[inherit]'>
               <div className='bg-white max-w-7xl md:mx-auto mx-6 p-6 mt-4 rounded'>
-                <div className='border-b border-gray-300'>
-                  <h3 className='text-lg pb-1 font-semibold text-gray-700'>Welcome!</h3>
+                <div className='border-b border-gray-200 mb-4'>
+                  <h3 className='text-lg pb-1.5 font-semibold text-gray-700'>Welcome!</h3>
+                </div>
+                <div className="w-full border border-gray-200 rounded-md flex flex-col md:flex-row items-center justify-between p-6">
+                  <div className="flex md:w-1/4 p-4 flex-col gap-3 items-center justify-center md:border-e border-gray-200">
+                    <div className="flex items-center justify-center gap-3">
+                      <LuNetwork size={30} className='text-blue-800' />
+                      <h3 className="text-xl font-semibold text-gray-600">8954</h3>
+                    </div>
+                    <span className='text-sm font-medium text-gray-400'>Lifetime total sales</span>
+                  </div>
+                  <div className="flex md:w-1/4 p-4 flex-col gap-3 items-center justify-center md:border-e border-gray-200">
+                    <div className="flex items-center justify-center gap-3">
+                      <FaChromecast size={30} className='text-green-800' />
+                      <h3 className="text-xl font-semibold text-gray-600">7542</h3>
+                    </div>
+                    <span className='text-sm font-medium text-gray-400'>Income amounts</span>
+                  </div>
+                  <div className="flex md:w-1/4 p-4 flex-col gap-3 items-center justify-center md:border-e border-gray-200">
+                    <div className="flex items-center justify-center gap-3">
+                      <FaUserShield size={30} className='text-blue-400' />
+                      <h3 className="text-xl font-semibold text-gray-600">6740</h3>
+                    </div>
+                    <span className='text-sm font-medium text-gray-400'>Total users</span>
+                  </div>
+                  <div className="flex md:w-1/4 p-4 flex-col gap-3 items-center justify-center">
+                    <div className="flex items-center justify-center gap-3">
+                      <RiCellphoneLine size={30} className='text-red-800' />
+                      <h3 className="text-xl font-semibold text-gray-600">3250</h3>
+                    </div>
+                    <span className='text-sm font-medium text-gray-400'>Total visits</span>
+                  </div>
                 </div>
               </div>
             </div>
